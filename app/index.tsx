94d17d9ac0760c032151134521b0244a916df165
pedro <pedrobldap41@gmail.com>
2025-11-28 21:21:20 -0300

    create initial app

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,71 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export default function Index() {
+
+
+export default function StartScreen() {
+  const router = useRouter();
+
+  const goToHome = () => {
+    router.push("/iniciar");
+  };
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/images/Logo.png")}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+
+      <Text style={styles.title}>Bem-vindo ao App!</Text>
+     
+      <TouchableOpacity style={styles.button} onPress={goToHome}>
+        <Text style={styles.buttonText}>Iniciar</Text>
+      </TouchableOpacity>
+       <Text style={styles.subtitle}>“Organize, personalize e monte cestas com facilidade.”</Text>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  logo: {
+    width: 250,
+    height: 250,
+  },
+  title: {
+    fontSize: 22,
+    marginTop: 20,
+    fontWeight: "bold",
+  },
+  button: {
+    marginTop: 40,
+    backgroundColor: "#e56a9bff",
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 25,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  subtitle: {
+    fontSize:20,
+    marginTop: 10,
+    fontWeight: "bold",
+    textAlign: "center",
+    position: "absolute",
+    bottom:50,
+    color:"#0000009e"
+  }
+});
