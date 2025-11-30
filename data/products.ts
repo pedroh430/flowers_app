9e070859d84bda5ec0fed81debd8f74a676cdd2b
pedro <pedrobldap41@gmail.com>
2025-11-30 18:13:40 -0300

    create list products

---
+++ data/products.ts
@@ -0,0 +1,35 @@
+import { Product } from "../types/Product";
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: "Cesta Romântica",
+    price: 120.00,
+    image: require("../assets/images/Logo.png"),
+  },
+  {
+    id:2,
+    name:"cesta",
+    price:44.55,
+    image: require("../assets/images/Logo.png")
+  },
+  {id:2,
+    name:"cesta",
+    price:44.55,
+    image: require("../assets/images/Logo.png")
+  },
+  {
+    id:2,
+    name:"cesta",
+    price:44.55,
+    image: require("../assets/images/Logo.png")
+  },
+  {
+    id:2,
+    name:"cesta",
+    price:44.55,
+    image: require("../assets/images/Logo.png")
+  }
+
+
+]
